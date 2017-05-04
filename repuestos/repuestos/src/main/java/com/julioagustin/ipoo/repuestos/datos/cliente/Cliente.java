fa765d881a3de9da29d749aeb0937dbb8a7b38fd
alumno-202 <alumno-202@M202-13.uaa.local>
2017-05-03 21:31:54 -0400

    Agregada la clase Cliente

---
+++ repuestos/repuestos/src/main/java/com/julioagustin/ipoo/repuestos/datos/cliente/Cliente.java
@@ -0,0 +1,16 @@
+package com.julioagustin.ipoo.repuestos.datos.cliente;
+
+// Inicio de la clase
+// nomenclatura: visibilidad class nombre de la clase
+public class Cliente {
+	
+	//atributos
+	private int id;
+	private String nroDoc;
+	private String tipoDoc;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	private String password;
+}
+// fin de la clase
