efa3653c8e06fbfef7818a9c85dad2259c7415f9
juliosanchez <juliosanchez@INF02.credisolucion.local>
2017-06-29 16:46:25 -0400

    Commit del segundo programa.

---
+++ aprederProgramarJavaDeitel/src/main/java/programarJavaDeitel/Bienvenido1.java
@@ -0,0 +1,14 @@
+package programarJavaDeitel; // Paquete del programa
+
+
+// Programa para imprimir un texto
+public class Bienvenido1 {
+	
+    // Método que ejecuta la clase
+	public static void main(String[] args) {
+		System.out.println("¡Bienvenido a la programación en Java!");
+
+	} // Fin del método main
+	
+	
+} // Fin de la clase
---
+++ aprederProgramarJavaDeitel/src/main/java/programarJavaDeitel/Bienvenido2.java
@@ -0,0 +1,15 @@
+package programarJavaDeitel; // Paquete del programa
+
+
+// Programa para imprimir un texto
+public class Bienvenido2 {
+	
+    // Método que ejecuta la clase
+	public static void main(String[] args) {
+		System.out.print("¡Bienvenido a ");
+		System.out.println("la programación en Java!");
+
+	} // Fin del método main
+	
+	
+} // Fin de la clase
